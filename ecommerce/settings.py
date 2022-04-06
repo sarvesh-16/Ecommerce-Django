24d33c4372d37e04aebe8fceb11a19d92f891c7c
sarvesh-16 <sarvesh.kale125@gmail.com>
2022-04-06 10:52:26 +0530

    host

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -14,8 +14,8 @@ import os
 import django
 import dj_database_url
 from django.db import DatabaseError 
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DatabaseError['default'].update(prod_db)
+# prod_db  =  dj_database_url.config(conn_max_age=500)
+# DatabaseError['default'].update(prod_db)
 # import django_heroku
 
 
@@ -49,8 +49,7 @@ INSTALLED_APPS = [
     'accounts',
     'social_django',
     'crispy_forms',
-    'rest_framework',
-    'herokuapp'
+    'rest_framework'
 ]
 
 AUTHENTICATION_BACKENDS = (
